91ef8bd9c6cae363e1668ff128f4d7da1b55f18a
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-07-27 17:20:50 +0200

    fine (?)

--- main.php
+++ main.php
@@ -59,9 +59,23 @@ $faq = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Google FAQ</title>
 </head>
 <body>
+
+<div class="container">
+    <?php
+
+    foreach ($faq as $value) {
+        
+        echo "<h2>" . $value["domanda"] . "</h2>";
+
+        echo "<p>" . $value["risposta"] . "</p>";
+    }
+
+    ?>
+</div>
     
 </body>
 </html>
